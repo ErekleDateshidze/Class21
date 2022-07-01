8f4b99482162aeed8cb37c0c3c4dff542a9154b5
Erekle99 <erekledateshidze99@gmail.com>
2022-07-01 14:57:46 +0400

    1
    1

--- script.js
+++ script.js
@@ -21,12 +21,13 @@ toggle.addEventListener('click', (e) => {
 })
 
 
-function setTIme() {
+function setTime() {
     const time = new Date()
     const month = time.getMonth()
     const day = time.getDay()
+    const date = time.getDate()
     const hour = time.getHours()
-    const hoursForClok = hour >= 13 ? hour % 12 : hours;
+    const hoursForClock = hour >= 13 ? hour % 12 : hour;
     const minutes = time.getMinutes()
     const second = time.getSeconds()
     const ampm = hour >= 12 ? "PM" : "AM"
@@ -34,4 +35,14 @@ function setTIme() {
     hourEl.style.transform = `translate(-50%,-100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
     minuteEl.style.transform = `translate(-50%,-100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
     secondEl.style.transform = `translate(-50%,-100%) rotate(${scale(second, 0, 60, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]} , ${months[month]} <span class="circle"> ${date} </span>`
 }
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min)
+}
+
+setTime()
+
+setInterval(setTime, 1000)
